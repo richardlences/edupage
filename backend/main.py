c940b1ed3bef1a729b792132c0fc95dc954fc57a
Richard Lences <richardlences0@gmail.com>
2025-12-17 20:00:37 +0100

    add missing import

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from models import User, Rating, Photo # Ensure models are registered
+import os
 
 app = FastAPI()
 
